36c73f0ac637d9a000f8f9d87f3b2b8ef9a0e07d
Michael Amos <zuzuspetals@hotmail.co.uk>
2017-10-20 08:55:01 -0400

    15 add date published to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017143202) do
+ActiveRecord::Schema.define(version: 20171020121433) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171017143202) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "published_on"
   end
 
 end
